879a56b83c13b8f86783af2e553699d16b74c31b
alexhorton1995 <alex.d.horton95@gmail.com>
2020-01-15 22:33:25 -0600

    Added for loop as well as some other logic to module1

---
+++ HelloWorldPython/module1.py
@@ -0,0 +1,18 @@
+student_names = ["James", "John", "Mark", "Paul", "Jesse", "Hannah", "Boaz", "Ruth", "Jesus"]
+
+for name in student_names:
+    if name == "Ruth":
+        continue
+        print("Found em! " + name)
+    print("Currently testing " + name)
+
+x = 0
+while x <= 128:
+    print ("x = {0}".format(x))
+    x += 1
+    pass
+
+
+
+
+     
